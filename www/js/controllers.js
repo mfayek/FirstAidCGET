db3cbb7e5b3fb2e8efcc4e0ca2171ea88883b0a1
unknown <mohamed.fayek@hotmail.com>
2015-02-04 14:47:59 -0500

    removed clearHistory from controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,11 @@
 myApp.controller('EmergenciesCtrl', function($ionicHistory){
-  $ionicHistory.clearHistory();
+
 });
 
 myApp.controller('AboutCtrl', function($ionicHistory){
-  $ionicHistory.clearHistory();
+
 });
 
 myApp.controller('HomeTabCtrl', function($scope,$ionicHistory) {
   
-    $ionicHistory.clearHistory();
 });
